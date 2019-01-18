a6fac2ce1cd000287dba0fd617f1bbdf797ed530
Evgeniya Naumova <jainnaumova@gmail.com>
2019-01-18 17:32:59 -0500

    func

--- bubblesort.js
+++ bubblesort.js
@@ -2,5 +2,23 @@ function bubbleSort(array) {
   /* your code here */
   if (array.length < 2) {
     return array;
+  } else {
+    for (let i = array.length - 1; i >= 0; i--) {
+      for (let j = 1; (j = i); j++) {
+        if (array[j - 1] > array[i]) {
+          let temp = array[j - 1];
+          array[j - 1] = array[i];
+          array[i] = temp;
+        }
+      }
+    }
   }
+  return array;
+}
+function swap(num1, num2) {
+  let arr = [num1, num2];
+  if (num2 < num1) {
+    arr[(num2, num1)];
+  }
+  return arr;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,41 +1,43 @@
+describe("swap", function() {
+  it("swap two items", function() {
+    expect(swap(3, 5)).toEqual([3, 5]);
+  });
+});
+
 describe("Bubble Sort", function() {
-  let swapCounter,
-  beforeEach(() => {
-    swapCounter = 0
+  beforeAll(function() {
+    it("swap two items", function() {
+      expect(swapCounter).toEqual(0);
+      expect(comparisonCounter).toEqual(0);
+      expect(bubbleSort([])).toEqual([]);
+    });
+    spyOn(swap).and.callThrough(); // replace existing method
+  });
+  it("getting to the center of swap involves exactly three licks", function() {
+    swap = function() {};
+    expect(swap.calls.count()).toEqual(0);
   });
 
-  let comparisonCounter,
-    beforeEach(() => {
-      comparisonCounter = 0
-  });
-
-  it("handles an empty array", function () {
-    expect(swapCounter).toEqual(0)
-    expect(comparisonCounter).toEqual(0)
+  it("handles an empty array", function() {
+    expect(swapCounter).toEqual(0);
+    expect(comparisonCounter).toEqual(0);
     expect(bubbleSort([])).toEqual([]);
   });
 
   it("handles an one item array", function() {
-    expect(swapCounter).toEqual(0)
-    expect(comparisonCounter).toEqual(0)
+    expect(swapCounter).toEqual(0);
+    expect(comparisonCounter).toEqual(0);
     expect(bubbleSort([2])).toEqual([2]);
   });
 
-  it("correctly sorts an array", function () {
-    expect(swapCounter).toEqual()
-    expect(comparisonCounter).toEqual()
+  it("correctly sorts an array", function() {
+    expect(swapCounter).toEqual();
+    expect(comparisonCounter).toEqual();
     expect(bubbleSort([8, 5, 3, 1, 9])).toEqual([1, 3, 5, 8, 9]);
   });
 });
 
-[8, 5, 3, 1, 9]
-
-[5, 8, 3, 1, 9]
-
-[5, 3, 8, 1, 9]
-
-
+[8, 5, 3, 1, 9][(5, 8, 3, 1, 9)][(5, 3, 8, 1, 9)];
 
 // swapCounter = 2
 // comparisonCounter = 2
-
